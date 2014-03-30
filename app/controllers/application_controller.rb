783985a09f28ccecbd6498adbd846496a961823e
Dimitri Kurashvili <dimakura@gmail.com>
2014-03-30 14:58:56 +0400

    protecting pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :validate_permission
+
+  def current_user; @_curr_user ||= (Sys::User.find(session[:user_id]) rescue nil) if session[:user_id] end
+  helper_method :current_user
+
+  def render(*args)
+    nav if self.respond_to?(:nav)
+    super
+  end
+
+  private
+
+  def validate_permission
+    unless Sys::Permission.has_permission?(current_user, controller_path, action_name)
+      if current_user 
+        render text: t('pages.application.validate_permission.no_permission')
+      else
+        redirect_to login_url, alert: t('pages.application.validate_permission.login_required')
+      end
+    end
+  end
 end
