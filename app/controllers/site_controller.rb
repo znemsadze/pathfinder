6e8416eb211daae794435c8470f59ae8c6ba4c0c
Dimitri Kurashvili <dimakura@gmail.com>
2014-03-30 16:16:10 +0400

    login page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,21 @@
 # -*- encoding : utf-8 -*-
 class SiteController < ApplicationController
-  def index
-    @title = t('pages.site.index.title')
+  def index; @title=t('pages.site.index.title') end
+
+  def login
+    @title=t('pages.site.login.title')
+    if request.post?
+      user = Sys::User.authenticate(params[:username], params[:password])
+      if user and user.active 
+        session[:user_id] = user.id
+        redirect_to root_url
+      else @error = t('pages.site.login.illegal_login') end
+    end
+  end
+
+  protected
+  def nav
+    @nav=super
+    @nav[@title]=nil unless action_name == 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Pathfinder::Application.routes.draw do
   scope controller: 'site' do
     get '/', action: 'index', as: 'home'
+    match '/login', action: 'login', as: 'login', via: ['get','post']
   end
 
   root 'site#index'
