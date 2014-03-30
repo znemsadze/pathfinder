21f23404c6af2c551678ac1213b4d446a7b08ba5
Dimitri Kurashvili <dimakura@gmail.com>
2014-03-30 13:51:05 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pathfinder::Application.config.secret_key_base = 'b9141130a25cb767ee1229e498fde84b7c895331ab89f9aab716b28e1e2d3ac487c78621f1c164f772d5d930082f69094d7f05608ee1a0a5aaef57da3a2f4afc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pathfinder::Application.config.session_store :cookie_store, key: '_pathfinder_session'
