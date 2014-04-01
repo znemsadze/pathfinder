d3a31f8caf1a9362523a3a9982cda419694740fe
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-01 11:39:45 +0400

    logout action

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,12 +5,17 @@ class SiteController < ApplicationController
   def login
     @title=t('pages.site.login.title')
     if request.post?
-      user = Sys::User.authenticate(params[:username], params[:password])
+      user=Sys::User.authenticate(params[:username], params[:password])
       if user and user.active 
-        session[:user_id] = user.id
+        session[:user_id]=user.id
         redirect_to root_url
-      else @error = t('pages.site.login.illegal_login') end
+      else @error=t('pages.site.login.illegal_login') end
     end
+  end
+
+  def logout
+    session[:user_id]=nil
+    redirect_to root_url
   end
 
   protected
