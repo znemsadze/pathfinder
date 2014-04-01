31de719488e23ed26594866bb203468d88f6a707
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-01 12:19:53 +0400

    change profile data

--- app/controllers/account/profile_controller.rb
+++ app/controllers/account/profile_controller.rb
@@ -2,6 +2,16 @@
 class Account::ProfileController < ApplicationController
   def index; @title=t('pages.account_profile.index.title') end
 
+  def edit
+    @title=t('pages.account_profile.edit.title')
+    @user=current_user
+    if request.post?
+      if @user.update_attributes(params.require(:sys_user).permit(:first_name,:last_name,:mobile))
+        redirect_to account_profile_url, notice: t('pages.account_profile.edit.saved')
+      end
+    end
+  end
+
   protected
   def nav
     @nav=super
