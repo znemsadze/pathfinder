3dec40a3038cc258ba9848c1a2d62859cde81101
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-29 13:09:30 +0400

    editing path API method

--- app/controllers/api/geo_controller.rb
+++ app/controllers/api/geo_controller.rb
@@ -3,9 +3,9 @@ class Api::GeoController < ApiController
     path=Geo::Path.create
     points=params[:points].map{|k,v| v}
     points.each do |p|
-      lat=p[:lat].to_f ; lng=p[:lng].to_f
-      if p[:featureId]
-        point=Geo::Path.find(p[:featureId]).points.where(lat:lat,lng:lng).first
+      lat=p[:lat].to_f ; lng=p[:lng].to_f ; featureId=p[:featureId]
+      if featureId
+        point=Geo::Path.find(featureId).points.where(lat:lat,lng:lng).first
         point.single=false ; point.save
       else
         point=Geo::Point.create(lat:lat,lng:lng,single:true)
@@ -16,8 +16,21 @@ class Api::GeoController < ApiController
   end
 
   def edit_path
-    path=Geo::Path.find(params[:id])
-    # TODO:
+    path=Geo::Path.find(params[:id]) ; path.point_ids=[] ; path.save
+    points=params[:points].map{|k,v| v}
+    points.each do |p|
+      id=p[:id] ; lat=p[:lat].to_f ; lng=p[:lng].to_f ; featureId=p[:featureId]
+      if id
+        point=Geo::Point.find(id)
+        point.lat=lat ; point.lng=lng ; point.save
+      elsif featureId
+        point=Geo::Path.find(featureId).points.where(lat:lat,lng:lng).first
+        point.single=false ; point.save
+      else
+        point=Geo::Point.create(lat:lat,lng:lng,single:true)
+      end
+      path.points<<point
+    end
     render json:{id:path.id.to_s}
   end
 end
