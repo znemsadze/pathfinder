aca4a334699ccf62b9517149c8751d3dbe02768f
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-29 16:09:11 +0400

    Geo::Point.single? method

--- app/models/geo/point.rb
+++ app/models/geo/point.rb
@@ -3,6 +3,7 @@ class Geo::Point
   include Mongoid::Document
   field :lat, type: Float
   field :lng, type: Float
-  field :single, type: Boolean, default: true
   has_and_belongs_to_many :paths, class_name:'Geo::Path'
+
+  def single?; self.paths.count<2 end
 end
