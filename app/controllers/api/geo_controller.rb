cb963ceb921c8043d2f94b41e26cfa21306b1b12
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-29 19:02:23 +0400

    first version of edit path routine

--- app/controllers/api/geo_controller.rb
+++ app/controllers/api/geo_controller.rb
@@ -1,9 +1,9 @@
 class Api::GeoController < ApiController
   def new_path
     path=Geo::Path.create
-    points=params[:points].map{|k,v| v}
-    points.each do |p|
-      lat=p['lat'].to_f ; lng=p['lng'].to_f
+    parameter_points=params[:points].map{|k,v| v}
+    parameter_points.each do |p|
+      lat,lng=p['lat'].to_f,p['lng'].to_f
       point=Geo::Point.where(lat:lat,lng:lng).first || Geo::Point.create(lat:lat,lng:lng)
       path.points<<point
     end
@@ -13,17 +13,16 @@ class Api::GeoController < ApiController
   end
 
   def edit_path
-    path=Geo::Path.find(params[:id]) ; path.point_ids=[] ; path.save
-    points=params[:points].map{|k,v| v}
-    points.each do |p|
-      id=p['id'] ; lat=p['lat'].to_f ; lng=p['lng'].to_f ; featureId=p['featureId']
-      if id.present?
-        point=Geo::Point.find(id)
+    parameter_points=params[:points].map{|k,v| v}
+    path=Geo::Path.find(params[:id])
+    points=path.ordered_points ; path.point_ids=[] ; path.save
+    parameter_points.each_with_index do |p,i|
+      lat=p['lat'].to_f ; lng=p['lng'].to_f
+      if i==0 or i==parameter_points.length-1
+        point=(i==0 ? points[0] : points[points.length-1])
         point.lat=lat ; point.lng=lng ; point.save
-      elsif featureId
-        point=Geo::Path.find(featureId).points.where(lat:lat,lng:lng).first
       else
-        point=Geo::Point.create(lat:lat,lng:lng)
+        point=Geo::Point.where(lat:lat,lng:lng).first || Geo::Point.create(lat:lat,lng:lng)
       end
       path.points<<point
     end
