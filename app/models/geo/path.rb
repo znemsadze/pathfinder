41ce846393ecd89c534c04ad05c6ce0c1ec161f6
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-29 16:07:02 +0400

    remove old fields from Geo::Path

--- app/models/geo/path.rb
+++ app/models/geo/path.rb
@@ -1,9 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Geo::Path
   include Mongoid::Document
-  field :startid, type: Moped::BSON::ObjectId
-  field :endid, type: Moped::BSON::ObjectId
-  field :length, type: Float, default: 0
   has_and_belongs_to_many :points, class_name: 'Geo::Point'
 
   def ordered_points; self.point_ids.map{|x|self.points.find(x)} end
