0fa223a6490f6bbebf66531312451fa61c02ff39
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-02 12:48:51 +0400

    splitjoin first iteration

--- app/models/geo/path.rb
+++ app/models/geo/path.rb
@@ -2,7 +2,6 @@
 class Geo::Path
   include Mongoid::Document
   field :point_ids, type: Array, default:[]
-  def points; Geo::Point.in(id:self.point_ids) end
 
   def self.new_path(points)
     if points.uniq.size>1
@@ -23,5 +22,76 @@ class Geo::Path
       path.save
       path
     end
+  end
+
+  def points; Geo::Point.in(id:self.point_ids) end
+
+  def edge?(p)
+    id=p.is_a?(Point) ? p.id : p
+    point_ids.first==id or point_ids.last==id
+  end
+
+  def splitjoin
+    self.point_ids.each do |point_id|
+      point=Geo::Point.find(point_id)
+      route_count=point.route_count
+      if point.path_count>1
+        if route_count==2
+          joinat(point)
+        else
+          # TODO:
+        end
+      end
+    end
+  end
+
+  private
+
+  def joinat(point)
+    # assume route_count == 2
+    path1=self
+    path2=Geo::Path.find(point.path_ids.select{|x|x.id!=self.id}.first)
+    edge1=path1.edge?(point)
+    edge2=path2.edge?(point)
+
+    if edge2 # second path's edge
+      addtoend=true
+      if not edge1
+        new_points=[point]
+      else
+        idx=path2.point_ids.index(point.id)
+        if idx==0
+          new_points=path2.points
+        else
+          new_points=path2.points.reverse
+        end
+      end
+    else # we are in the middle of the secon path
+      addtoend=false
+      points2=path2.points
+      idx=path2.point_ids.index(point.id)
+      if points2[idx-1].path_ids.contains?(path1.id)
+        new_points=points2[0..idx]
+      else
+        new_points=points2[idx..-1].reverse
+      end
+    end
+
+    new_points.each do |point|
+      point.path_ids.delete(path2.id)
+      point.path_ids.push(path1.id) unless point.path_ids.include?(path1.id)
+      point.save
+      path2.point_ids.delete(point.id)
+      path2.save
+    end
+
+    path2.destroy unless path2.point_ids.any?
+
+    if addtoend
+      path1.point_ids=(path1.point_ids+new_points).uniq
+    else
+      path1.point_ids=(new_points+path1.point_ids).uniq
+    end
+    path1.save
   end
 end
--- test/models/geo/path_test.rb
+++ test/models/geo/path_test.rb
@@ -8,5 +8,17 @@ class Geo::PathTest < ActiveSupport::TestCase
     assert_equal 0, Geo::Point.count
   end
 
-  
+  test 'simplest splitjoin' do
+    clear_db
+    p1=Geo::Point.create(lat: 1,lng: 1)
+    p2=Geo::Point.create(lat: 1,lng: 2)
+    p3=Geo::Point.create(lat: 1,lng: 3)
+    p4=Geo::Point.create(lat: 1,lng: 4)
+    path1=Geo::Path.new_path([p1,p2,p3])
+    path2=Geo::Path.new_path([p2,p3,p4])
+    assert_equal 2, Geo::Path.count
+    assert_equal 4, Geo::Point.count
+    path1.splitjoin
+    assert_equal 4, path1.points.size
+  end
 end
