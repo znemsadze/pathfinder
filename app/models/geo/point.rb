4fc79c17d1cf6ad7556cf829319c148eb171ea5f
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-02 10:39:44 +0400

    point parametrization

--- app/models/geo/point.rb
+++ app/models/geo/point.rb
@@ -4,4 +4,18 @@ class Geo::Point
   field :lat, type: Float
   field :lng, type: Float
   field :path_ids, type: Array
+
+  def path_count; self.path_ids.size end
+  def route_count; self.neighbours.size end
+
+  def neighbours
+    points=[]
+    self.path_ids.each do |path_id|
+      path=Geo::Path.find(path_id)
+      idx=path.point_ids.index(self.id)
+      points<<path.point_ids[idx-1] if (idx>0)
+      points<<path.point_ids[idx+1] if (idx<path.point_ids.size-1)
+    end
+    points.uniq
+  end
 end
