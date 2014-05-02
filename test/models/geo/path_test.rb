8be57bf904a52725ffc32dd6277a2ff6c1d9f950
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-02 14:24:27 +0400

    more general test

--- test/models/geo/path_test.rb
+++ test/models/geo/path_test.rb
@@ -8,17 +8,32 @@ class Geo::PathTest < ActiveSupport::TestCase
     assert_equal 0, Geo::Point.count
   end
 
-  test 'simplest splitjoin' do
+  def splitjoin_testing(paths,results)
     clear_db
-    p1=Geo::Point.create(lat: 1,lng: 1)
-    p2=Geo::Point.create(lat: 1,lng: 2)
-    p3=Geo::Point.create(lat: 1,lng: 3)
-    p4=Geo::Point.create(lat: 1,lng: 4)
-    path1=Geo::Path.new_path([p1,p2,p3])
-    path2=Geo::Path.new_path([p2,p3,p4])
-    assert_equal 2, Geo::Path.count
-    assert_equal 4, Geo::Point.count
-    path1.splitjoin
-    assert_equal 4, path1.points.size
+    paths.each {|path| Geo::Path.new_path(path)}
+    assert_equal paths.count, Geo::Path.count
+    assert_equal paths.flatten.uniq.size, Geo::Point.count
+    Geo::Path.first.splitjoin
+    paths=Geo::Path.all
+    assert_equal results.size, paths.size
+    results.each_with_index do |result,i|
+      path=paths[i]
+      assert_equal path.point_ids.size, result.size
+      result.each_with_index do |corrdinates,j|
+        point=Geo::Point.find(path.point_ids[j])
+        assert_equal point.lat, corrdinates[0]
+        assert_equal point.lng, corrdinates[1]
+      end
+    end
+  end
+
+  test 'simplest splitjoin' do
+    p1=[1,1] ; p2=[1,2] ; p3=[1,3] ; p4=[1,4]
+
+    # (p1,p2,p3) + (p2,p3,p4) -> (p1,p2,p3,p4)
+    splitjoin_testing([[p1,p2,p3],[p2,p3,p4]],[[p1,p2,p3,p4]])
+    # (p2,p3,p4) + (p1,p2,p3) -> (p1,p2,p3,p4)
+    splitjoin_testing([[p2,p3,p4],[p1,p2,p3]],[[p1,p2,p3,p4]])
+
   end
 end
