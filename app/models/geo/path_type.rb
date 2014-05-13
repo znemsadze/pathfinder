5a10b5a21f9fd8ccace2c0c99fedf2c41d1f6f2c
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-13 12:08:19 +0400

    change ordering for Geo::PathType

--- app/models/geo/path_type.rb
+++ app/models/geo/path_type.rb
@@ -17,4 +17,24 @@ class Geo::PathType
       t.order_by=idx+offset+1 ; t.save
     end
   end
+
+  def up
+    if self.order_by>1
+      type=Geo::PathType.where(order_by:self.order_by-1).first
+      type.order_by=self.order_by
+      type.save
+      self.order_by=self.order_by-1
+      self.save
+    end
+  end
+
+  def down
+    if self.order_by<Geo::PathType.count
+      type=Geo::PathType.where(order_by:self.order_by+1).first
+      type.order_by=self.order_by
+      type.save
+      self.order_by=self.order_by+1
+      self.save
+    end
+  end
 end
