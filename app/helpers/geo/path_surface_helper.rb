2557ebce6e2040187c91e266a48bf021b4706d2f
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-14 13:37:48 +0400

    show relation for path surface

--- app/helpers/geo/path_surface_helper.rb
+++ app/helpers/geo/path_surface_helper.rb
@@ -14,16 +14,23 @@ module Geo::PathSurfaceHelper
     end
   end
 
-  def pathsurface_view(surface)
-    view_for surface, title: 'სახეობის თვისება', icon: '/icons/car.png', collapsible: true do |f|
+  def pathsurface_view(surface,opts={})
+    idx=case opts[:tab] when 'sys' then 2 when 'details' then 1 else 0 end
+    view_for surface, title: 'სახეობის თვისება', icon: '/icons/car.png', collapsible: true, selected_tab: idx do |f|
       f.edit_action geo_edit_path_surface_url(id:surface.id)
       f.delete_action geo_delete_path_surface_url(id:surface.id)
       f.tab title: 'ძირითადი', icon: '/icons/car.png' do |f|
-        f.complex_field required: true, i18n: 'order_by' do |c|
-          c.text_field 'type.name', after: '&mdash;'.html_safe, url: geo_path_type_url(id:surface.type_id)
-          c.text_field 'order_by', tag: 'strong'
-        end
+        f.text_field 'order_by', tag: 'strong', required: true
+        f.text_field 'type.name', required:true, i18n:'type', url: geo_path_type_url(id:surface.type_id)
         f.text_field 'name', required: true
+      end
+      f.tab title: "დეტალები &mdash; <strong>#{surface.details.count}</strong>".html_safe, icon: '/icons/magnifier.png' do |f|
+        f.table_field 'details', table: {title: 'გზის საფარის დეტალები', icon: '/icons/magnifier.png'} do |f|
+          f.table do |t|
+            t.text_field 'order_by', tag: 'strong'
+            t.text_field 'name', url: ->(x){geo_path_detail_url(id:x.id)}
+          end
+        end
       end
       f.tab title: 'სისტემური', icon: '/icons/traffic-cone.png' do |f|
         f.timestamps
