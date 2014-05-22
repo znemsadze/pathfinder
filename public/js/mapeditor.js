6d4bee896110fd65492478086b0afa2cdb11edc9
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-22 12:06:10 +0400

    fixing combos

--- public/js/mapeditor.js
+++ public/js/mapeditor.js
@@ -354,9 +354,9 @@ exports.form=function(opts){
   var saveAction={label: 'გზის შენახვა', icon:'save', type:'success', action: save_f};
   var cancelAction={label:'გაუმება', icon:'times-circle', action: cancel_f};
 
-  var typeCombo=ui.form.comboField('type_id', {label: 'გზის სახეობა', collection_url: '/geo/pathtype.json', text_property: 'name'});
-  var surfaceCombo=ui.form.comboField('surface_id', {label: 'გზის საფარი', collection_url: '/geo/pathsurface.json', text_property: 'name', parent_combo: typeCombo, parent_key: 'type_id'});
-  var detailsCombo=ui.form.comboField('detail_id', {label: 'საფარის დეტალები', collection_url: '/geo/pathdetail.json', text_property: 'name', parent_combo: surfaceCombo, parent_key: 'surface_id'});
+  var typeCombo=ui.form.comboField('type_id', {label: 'გზის სახეობა', collection_url: '/objects/path/types.json', text_property: 'name'});
+  var surfaceCombo=ui.form.comboField('surface_id', {label: 'გზის საფარი', collection_url: '/objects/path/surfaces.json', text_property: 'name', parent_combo: typeCombo, parent_key: 'type_id'});
+  var detailsCombo=ui.form.comboField('detail_id', {label: 'საფარის დეტალები', collection_url: '/objects/path/details.json', text_property: 'name', parent_combo: surfaceCombo, parent_key: 'surface_id'});
   var descriptionText=ui.form.textArea('description', {label: 'შენიშვნები'});
 
   var fields=[typeCombo, surfaceCombo, detailsCombo,descriptionText];
