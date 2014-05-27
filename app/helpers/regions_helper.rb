0e4bd3cad588af028392b54757e896aa5e983c7a
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-27 17:02:22 +0400

    linking line to region

--- app/helpers/regions_helper.rb
+++ app/helpers/regions_helper.rb
@@ -14,12 +14,20 @@ module RegionsHelper
 
   def region_view(region,opts={})
     title='რეგიონის თვისებები'; icon='/icons/region.png'
-    view_for region, title: title, collapsible: true, icon: icon do |v|
+    idx=case opts[:tab] when 'lines' then 1 when 'sys' then 2 else 0 end
+    view_for region, title: title, collapsible: true, icon: icon, selected_tab: idx do |v|
       v.edit_action edit_region_url(id:region.id)
       v.delete_action delete_region_url(id:region.id)
       v.tab title: 'ზოგადი', icon:icon do |v|
         v.text_field 'name', required: true
         v.text_field 'description'
+      end
+      v.tab title: "ხაზები &mdash; <strong>#{region.lines.count}</strong>".html_safe, icon: '/icons/line.png' do |v|
+        v.table_field 'lines', table: {title:'ხაზები', icon: '/icons/line.png'} do |field|
+          field.table do |t|
+            t.text_field 'name', url: ->(x){objects_line_url(id:x.id)}
+          end
+        end
       end
       v.tab title: 'სისტემური',icon:'/icons/traffic-cone.png' do |v|
         v.timestamps
