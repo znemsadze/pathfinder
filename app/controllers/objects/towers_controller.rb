1cf451ddc7167ec176621ed133a6ab07b75797eb
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-28 16:19:49 +0400

    upload excel file for towers

--- app/controllers/objects/towers_controller.rb
+++ app/controllers/objects/towers_controller.rb
@@ -10,16 +10,14 @@ class Objects::TowersController < ApplicationController
   end
 
   def upload
-    @title='KMZ ფაილის ატვირთვა'
+    @title='ფაილის ატვირთვა'
     if request.post?
-      Zip::File.open params[:data].tempfile do |zip_file|
-        zip_file.each do |entry|
-          if 'kml'==entry.name[-3..-1]
-            kml=entry.get_input_stream.read
-            Objects::Tower.from_kml(kml)
-          end
-        end
-      end
+      f=params[:data].original_filename
+      case File.extname(f).downcase
+      when '.kmz' then upload_kmz(params[:data].tempfile)
+      when '.kml' then upload_kml(params[:data].tempfile)
+      when '.xlsx' then upload_xlsx(params[:data].tempfile)
+      else raise 'არასწორი ფორმატი' end
       redirect_to objects_towers_url, notice: 'მონაცემები ატვირთულია'
     end
   end
@@ -34,5 +32,34 @@ class Objects::TowersController < ApplicationController
     @nav=super
     @nav['ანძები']=objects_towers_url
     @nav[@title]=nil unless ['index'].include?(action_name)
+  end
+
+  private
+
+  def upload_kmz(file)
+    Zip::File.open file do |zip_file|
+      zip_file.each do |entry|
+        upload_kml(entry) if 'kml'==entry.name[-3..-1]
+      end
+    end
+  end
+
+  def upload_kml(file)
+    kml=file.get_input_stream.read
+    Objects::Tower.from_kml(kml)
+  end
+
+  def upload_xlsx(file)
+    sheet=Roo::Spreadsheet.open(file.path, extension: 'xlsx')
+    (2..sheet.last_row).each do |row|
+      id=sheet.cell('A',row) ; name=sheet.excelx_value('C',row).to_s ; regionname=sheet.cell('D',row).to_s
+      lat=sheet.cell('E',row).to_f; lng=sheet.cell('F',row).to_f
+      region=Region.where(name:regionname).first
+      region=Region.create(name:regionname) unless region.present?
+      tower=Objects::Tower.find(id)
+      tower.name=name ; tower.region=region
+      tower.lat=lat ; tower.lng=lng
+      tower.save
+    end
   end
 end
