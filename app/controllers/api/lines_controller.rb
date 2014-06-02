84bb80962d3b22b2f7034ae4fe94fb8f2fd718f3
Dimitri Kurashvili <dimakura@gmail.com>
2014-06-02 09:45:18 +0400

    new action in Api::LinesController

--- app/controllers/api/lines_controller.rb
+++ app/controllers/api/lines_controller.rb
@@ -2,21 +2,18 @@
 class Api::LinesController < ApiController
   def show; @line=Objects::Line.find(params[:id]) end
 
-  # def new
-  #   parameter_points=params[:points].map{|k,v| [v['lat'].to_f,v['lng'].to_f]}
-  #   path=Objects::Path::Line.new_path(parameter_points,params)
-  #   path.splitjoin
-  #   render json:{id:path.neighbours.join(',')}
-  # end
+  def new
+    line=Objects::Line.create(params.permit(:name,:description,:region_id))
+    parameter_points=params[:points].map{|k,v| [v['lat'].to_f,v['lng'].to_f]}
+    line.set_points(parameter_points)
+    render json:{id:line.id.to_s}
+  end
 
   def edit
     line=Objects::Line.find(params[:id])
     parameter_points=params[:points].map{|k,v| [v['lat'].to_f,v['lng'].to_f]}
-    line.set_points(parameter_points)
-    line.name=params[:name]
-    line.description=params[:description]
-    line.region_id=params[:region_id]
-    line.save
+    line.set_points(parameter_points); line.save
+    line.update_attributes(params.permit(:name,:description,:region_id))
     render json:{id:line.id.to_s}
   end
 
