0a406b5f6ed7ac1432ac1ef7d6c0421e341067af
Dimitri Kurashvili <dimakura@gmail.com>
2014-06-02 15:38:10 +0400

    offices page

--- app/models/objects/office.rb
+++ app/models/objects/office.rb
@@ -2,7 +2,9 @@
 class Objects::Office
   include Mongoid::Document
   include Objects::Coordinate
+  include Objects::Kml
 
+  field :kmlid, type: String
   field :name, type: String
   field :description, type: String
   field :address, type: String
