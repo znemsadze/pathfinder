d275a800e0f054c364e00cf12c9e34c3d7e7f6b9
Dimitri Kurashvili <dimakura@gmail.com>
2014-06-26 14:04:01 +0400

    move to mongoid 4

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,14 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require "bson"
+require "moped"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+Moped::BSON = BSON
 
 module Pathfinder
   class Application < Rails::Application
