4e06c19c706266cf779e1515a920c07e14296736
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-06 17:32:29 +0400

    fixing turbolinks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,7 +3,7 @@ module TasksHelper
   def tasks_table(tasks, opts={})
     table_for tasks, title: opts[:title]||'დავალებები', icon: '/icons/report-paper.png', collapsible: true do |t|
       t.text_field :number, tag: 'code'
-      t.date_field 'created_at', url: ->(x){ tasks_task_url(id:x.id) }
+      t.date_field 'created_at', url: ->(x){ tasks_task_url(id:x.id) }, turbolink: false
       t.text_field 'status_name', i18n: 'status', icon: ->(x){ x.status_icon }
       t.text_field :note
       t.complex_field i18n: 'assignee' do |t|
