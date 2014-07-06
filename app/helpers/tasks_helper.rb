2710dc6e7771856eca7ad115359fa12a0a1c7be9
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-06 17:50:23 +0400

    add note to task view

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,10 +2,10 @@
 module TasksHelper
   def tasks_table(tasks, opts={})
     table_for tasks, title: opts[:title]||'დავალებები', icon: '/icons/report-paper.png', collapsible: true do |t|
-      t.text_field :number, tag: 'code'
+      t.text_field 'number', tag: 'code'
       t.date_field 'created_at', url: ->(x){ tasks_task_url(id:x.id) }, turbolink: false
       t.text_field 'status_name', i18n: 'status', icon: ->(x){ x.status_icon }
-      t.text_field :note
+      t.text_field 'note'
       t.complex_field i18n: 'assignee' do |t|
         t.text_field 'assignee.username', tag: 'strong', url: ->(x){ admin_user_url(id: x.assignee.id) }
         t.text_field 'assignee.full_name'
@@ -24,6 +24,7 @@ module TasksHelper
           f.text_field 'assignee.username', tag: 'strong', url: admin_user_url(id: task.assignee.id)
           f.text_field 'assignee.full_name'
         end
+        f.text_field 'note'
       end
       f.tab title: 'სისტემური',icon:'/icons/traffic-cone.png' do |f|
         f.timestamps
