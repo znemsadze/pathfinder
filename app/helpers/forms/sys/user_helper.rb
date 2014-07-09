677ff9e2839daceb648cb4c41c089f4b4a415cce
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-09 15:50:44 +0400

    change regions title

--- app/helpers/forms/sys/user_helper.rb
+++ app/helpers/forms/sys/user_helper.rb
@@ -31,9 +31,9 @@ module Forms::Sys::UserHelper
         f.boolean_field 'admin', required:true
         f.boolean_field 'active', required:true
       end
-      table_opts={title: 'რეგიონები', icon:'/icons/region.png'}
+      table_opts={ title: "რეგიონები &mdash; <strong>#{user.regions.count}</strong>".html_safe, icon:'/icons/region.png' }
       f.tab table_opts do |t|
-        t.table_field :regions, table:table_opts do |tbl|
+        t.table_field :regions, table: table_opts do |tbl|
           tbl.table do |t|
             t.title_action admin_user_add_region_url(id:user.id), label: 'რეგიონის დამატება', icon: '/icons/plus.png'
             t.delete_action ->(x){ admin_user_remove_region_url(id:user.id,role_id:x.id) }
