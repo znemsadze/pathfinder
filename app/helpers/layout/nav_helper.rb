e50fdc3f2117f3142c7d1c2cdc19bcb05e577035
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-09 12:31:58 +0400

    disable turbolinks on menu items

--- app/helpers/layout/nav_helper.rb
+++ app/helpers/layout/nav_helper.rb
@@ -5,7 +5,7 @@ module Layout::NavHelper
     if @nav and @nav.size > 1
       el('ul', attrs: {class: 'breadcrumb'}, children: @nav.each_with_index.map { |(lbl,url), i|
         if i == @nav.size - 1 then el('li', attrs: {class: 'active'}, text: lbl)
-        else el('li', children: [el('a', attrs: {href: url}, text: lbl)]) end
+        else el('li', children: [el('a', attrs: {href: url, 'data-no-turbolink' => true}, text: lbl)]) end
       }).to_s
     end
   end
