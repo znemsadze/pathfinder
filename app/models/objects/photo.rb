90c777e58dce53a32ff64c2646696e720646589f
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-22 21:31:56 +0400

    track photo time changes

--- app/models/objects/photo.rb
+++ app/models/objects/photo.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Objects::Photo
   include Mongoid::Document
+  include Mongoid::Timestamps
   belongs_to :owner, polymorphic: true
   field :filename, type: String
   field :confirmed, type: Mongoid::Boolean
