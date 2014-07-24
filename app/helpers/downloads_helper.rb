c708c5d413d2bf171b14c7423b609f8d97b8e0d7
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-24 10:38:02 +0400

    APK version 006

--- app/helpers/downloads_helper.rb
+++ app/helpers/downloads_helper.rb
@@ -1,7 +1,9 @@
 module DownloadsHelper
-  def android_button(version)
+  def android_button
+    version = Pathfinder::LATEST_APK_VERSION
     %Q{
-      <a href="/downloads/pathfinder-v#{version}.apk" class="btn btn-default btn-sm">
+      <a href="/downloads/pathfinder-v#{
+      version}.apk" class="btn btn-default btn-sm">
         <i class="fa fa-android"></i> Android აპლიკაცია (v#{version})
       </a>
     }.html_safe
