85720ca8023ab6f1567f1a92e4f48f764c87aa81
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-24 10:34:20 +0400

    change android links

--- app/helpers/downloads_helper.rb
+++ app/helpers/downloads_helper.rb
@@ -1,3 +1,9 @@
 module DownloadsHelper
-  def apk_url(version); "/downloads/pathfinder-v#{version}.apk" end
+  def android_button(version)
+    %Q{
+      <a href="/downloads/pathfinder-v#{version}.apk" class="btn btn-default btn-sm">
+        <i class="fa fa-android"></i> Android აპლიკაცია (v#{version})
+      </a>
+    }.html_safe
+  end
 end
