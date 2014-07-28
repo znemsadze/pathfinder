2a4bc181f78f986cef935ac7470ada151c5a56c5
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-28 15:58:54 +0400

    upload path lines in background job

---
+++ app/workers/kml_converter.rb
@@ -0,0 +1,11 @@
+# -*- encoding : utf-8 -*-
+class KMLConverter
+  include Sidekiq::Worker
+
+  def perform(type, path)
+    kml = File.read(path)
+    case type
+    when 'Objects::Path::Line' then Objects::Path::Line.from_kml(kml)
+    end
+  end
+end
