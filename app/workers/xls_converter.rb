d3b085330933804f9d71b6f166d11aefba66e7af
Dimitri Kurashvili <dimakura@gmail.com>
2014-08-02 18:01:39 +0400

    fixing region obtaining methods in xls_converter

--- app/workers/xls_converter.rb
+++ app/workers/xls_converter.rb
@@ -12,16 +12,10 @@ class XLSConverter
 
   private
 
-  def get_region(regionname)
-    region=Region.where(name:regionname).first
-    region=Region.create(name:regionname) unless region.present?
-    region
-  end
-
   def lines_converter(sheet)
     (2..sheet.last_row).each do |row|
       id=sheet.cell('A',row) ; name=sheet.cell('C',row) ; regionname=sheet.cell('D',row)
-      region = get_region(regionname)
+      region = Region.get_by_name(regionname)
       line = Objects::Line.find(id)
       line.name = name ; line.region = region ; line.save
     end
@@ -32,7 +26,7 @@ class XLSConverter
       id = sheet.cell('A',row) ; tower = Objects::Tower.find(id)
       name = sheet.excelx_value('B',row).to_s ; tower.name = name
       category=sheet.cell('C', row) ; tower.category = category
-      regionname = sheet.cell('D',row).to_s ; tower.region = get_region(regionname)
+      regionname = sheet.cell('D',row).to_s ; tower.region = Region.get_by_name(regionname)
       description=sheet.cell('E',row) ; tower.description = description
       tower.save
     end
