6c19faad86f5ce06c311b3520f892cc103dc4b98
Dimitri Kurashvili <dimakura@gmail.com>
2014-08-02 17:32:10 +0400

    towers to/from XML

--- app/workers/xls_converter.rb
+++ app/workers/xls_converter.rb
@@ -12,11 +12,16 @@ class XLSConverter
 
   private
 
+  def get_region(regionname)
+    region=Region.where(name:regionname).first
+    region=Region.create(name:regionname) unless region.present?
+    region
+  end
+
   def lines_converter(sheet)
     (2..sheet.last_row).each do |row|
       id=sheet.cell('A',row) ; name=sheet.cell('C',row) ; regionname=sheet.cell('D',row)
-      region=Region.where(name:regionname).first
-      region=Region.create(name:regionname) unless region.present?
+      region = get_region(regionname)
       line = Objects::Line.find(id)
       line.name = name ; line.region = region ; line.save
     end
@@ -24,15 +29,11 @@ class XLSConverter
 
   def towers_converter(sheet)
     (2..sheet.last_row).each do |row|
-      id=sheet.cell('A',row) ; name=sheet.excelx_value('C',row).to_s ; category=sheet.cell('D', row)
-      regionname=sheet.cell('E',row).to_s
-      lat=sheet.cell('F',row).to_f; lng=sheet.cell('G',row).to_f
-      description=sheet.cell('H',row)
-      region=Region.where(name:regionname).first
-      region=Region.create(name:regionname) unless region.present?
-      tower=Objects::Tower.find(id)
-      tower.name=name ; tower.region=region ; tower.category=category
-      tower.lat=lat ; tower.lng=lng; tower.description=description
+      id = sheet.cell('A',row) ; tower = Objects::Tower.find(id)
+      name = sheet.excelx_value('B',row).to_s ; tower.name = name
+      category=sheet.cell('C', row) ; tower.category = category
+      regionname = sheet.cell('D',row).to_s ; tower.region = get_region(regionname)
+      description=sheet.cell('E',row) ; tower.description = description
       tower.save
     end
   end
