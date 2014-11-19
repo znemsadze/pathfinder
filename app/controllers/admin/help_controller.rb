600e00498dda5aaad6039da36d866fdcb2e4edb8
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-19 21:39:57 +0400

    adding more help text

--- app/controllers/admin/help_controller.rb
+++ app/controllers/admin/help_controller.rb
@@ -7,6 +7,6 @@ class Admin::HelpController < ApplicationController
     @nav['დახმარება'] = admin_help_url
   end
 
-  # def login_required; true end
-  # def permission_required; not current_user.admin? end
+  def login_required; true end
+  def permission_required; not current_user.admin? end
 end
