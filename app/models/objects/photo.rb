175ebad8a66440e2c444c739df7d5f048341c0bc
Dimitri Kurashvili <dimakura@gmail.com>
2014-12-01 20:12:55 +0400

    change photo methods

--- app/models/objects/photo.rb
+++ app/models/objects/photo.rb
@@ -7,6 +7,6 @@ class Objects::Photo
   field :confirmed, type: Mongoid::Boolean
 
   def self.not_confirmed; Objects::Photo.where(confirmed: false) end
-  def thumbnail_url; "#{self.owner.thumb_url}/#{self.filename}" end
-  def large_url; "#{self.owner.large_url}/#{self.filename}" end
+  def thumbnail_url; "/uploads/#{self.owner_id}/thumb/#{self.filename}" end
+  def large_url; "/uploads/#{self.owner_id}/large/#{self.filename}" end
 end
